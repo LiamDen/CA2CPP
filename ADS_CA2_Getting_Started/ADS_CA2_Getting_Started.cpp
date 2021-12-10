490fd80f96f05014958523cbd246d6fea35c1384
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-12-10 08:53:21 +0000

    Initial commit which includes add, print, clear, getCount

--- ADS_CA2_Getting_Started/ADS_CA2_Getting_Started.cpp
+++ ADS_CA2_Getting_Started/ADS_CA2_Getting_Started.cpp
@@ -1,108 +1,32 @@
-// ADS_CA2_Getting_Started.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// 2021 - CA3 - Project - Q1 - BinaryTree
 
 #include <iostream>
+#include <exception>
 #include "ADS_CA2_Getting_Started.h"
 #include "BinaryTree.h"
 
-void demoSimpleArrayedBinaryTree();
-void demoPointerArrayedBinaryTree();
+void demoPointerArrayedBinaryTreeInts();
 
 int main()
 {
-	demoSimpleArrayedBinaryTree();
+	cout << endl << "ADS - 2021 - CA - Binary Tree..." << endl;
 
-	demoPointerArrayedBinaryTree();
+	cout << endl << "demoPointerArrayedBinaryTreeInts()..." << endl;
+	demoPointerArrayedBinaryTreeInts();
+
+	cout << endl << "Done..." << endl;
 }
 
-void demoPointerArrayedBinaryTree()
+void demoPointerArrayedBinaryTreeInts()
 {
-	int length = 20;
-	Node* tree[20];
+	//levels = 3, therefore capacity = 2^levels - 1 = 2^3 - 1 = 8 - 1 = 7
+	BinaryTree myTree(3, -1);
 
-	for (int i = 0; i < length; i++) {
-		tree[i] = nullptr;
-	}
+	myTree.add(5);
+	myTree.add(2);
+	myTree.add(4);
+	myTree.add(3);
+	myTree.add(1);
 
-	//add new data
-	int data = 20;
-	int position = 0;
-
-	//find the parent node
-	auto currentNode = tree[position];
-
-	//no parent
-	if (currentNode == nullptr)
-	{
-		Node n(20);
-		currentNode = &n;
-	}
-}
-
-void demoSimpleArrayedBinaryTree()
-{
-	int length = 20;
-	Node tree[20];
-	Node grown[40];
-
-	for (int i = 0; i < 20; i++) {
-		grown[i] = tree[i];
-	}
-
-	for (int i = 20; i < 40; i++) {
-		grown[i] = Node();
-	}
-
-	for (int i = 0; i < length; i++) {
-		tree[i] = Node();
-	}
-
-	//if less, look for left child
-	//else if more, look for right child
-
-	//add new data
-	int data = 20;
-	int position = 0;
-
-	//find the parent node
-	auto currentNode = tree[position];
-
-	//no parent
-	if (currentNode.data == -1)
-	{
-		currentNode.data = 20;
-	}
-	else
-	{
-		//if yes, compare data to parent
-		if (data <= currentNode.data)
-		{
-			//calculate the left node position
-			position = 2 * position + 1;
-
-			//check position >= capacity
-			//throw an exception and replace with grow()
-
-			//set left as new current
-			currentNode = tree[position];
-			if (currentNode.data == -1)
-			{
-				currentNode.data = data;
-			}
-		}
-		else
-		{
-			position = 2 * position + 2;
-
-			//check position >= capacity
-			//throw an exception and replace with grow()
-
-			//set left as new current
-			currentNode = tree[position];
-			if (currentNode.data == -1)
-			{
-				currentNode.data = data;
-			}
-		}
-	}
+	myTree.print();
 }
--- ADS_CA2_Getting_Started/BinaryTree.h
+++ ADS_CA2_Getting_Started/BinaryTree.h
@@ -1,137 +1,181 @@
 #include <iostream>
+#include <iomanip> //used for setw() in print()
 
-struct Node {
+using namespace std;
+
+/// @brief an example of something we could store in a Node, and hence a BinaryTree
+class Student {
 public:
-	int data; //name, age
+	int age;
 
-	Node(int data) : data(data) {};
-	Node() : data(-1) {};
+	Student(int age) : age(age) {
+	}
+	int getAge() {
+		return this->age;
+	}
+
+	/// @brief note how we must provide overridden versions of any operators used in the BinaryTree:add()
+	bool operator<=(const Student& rhs) {
+		return this->age <= rhs.age;
+	}
+
+	/// @brief note how we must provide overridden versions of any operators used in the BinaryTree:add()
+	bool operator==(const Student& rhs) {
+		return this->age == rhs.age;
+	}
 };
 
+/// @brief a node which stores data and is used by the BinaryTree class
+class Node {
+public:
+	int data;
+
+	Node() {};
+	Node(int data) : data(data) {};
+
+	/// @brief note how we must provide any overridden operators used in the BinaryTree::add() - see line 136
+	Node& operator=(const Node& rhs)
+	{
+		//copy
+		this->data = rhs.data;
+
+		//return refernce to Node (remember that "this" is a pointer so we must de-reference (using &) and get object)
+		return *this;
+	}
+
+	/// @brief print the node contents - used in print()!
+	friend ostream& operator<<(ostream& os, const Node& node) {
+		os << node.data;
+		return os;
+	}
+};
+
+/// @brief a binary tree based on an array implementation
 class BinaryTree
 {
 public:
 	//data array
 	Node* dataArray;
+	int defaultValue;
 
 	int count = 0;
 	int capacity = 0;
 
-	BinaryTree()
+	/// @brief instantiate and provide a default T so we can clear the array with some default/start value
+	BinaryTree(int numLevels, int defaultValue)
 	{
-		count = 0;
-		capacity = 15;
+		//save the vars
+		this->count = 0;
+		this->capacity = static_cast<int>(pow(2, numLevels) - 1);
+		this->defaultValue = defaultValue;
+
+		//make the array
 		dataArray = new Node[capacity];
+
+		//clear initial values
+		clear();
 	}
 
-	BinaryTree(int numLevels) //levels = 3
-	{
-		count = 0;
-		capacity = pow(2, numLevels) - 1;
-		dataArray = new Node[capacity];
-	}
-
+	/// @brief free arrar resources
 	~BinaryTree()
 	{
-		//for (int i = 0; i < capacity; i++)
-		//{
-		//	//set to nullptr
-		//}
-
-		//delete[] dataArray;
-		//capacity = 0;
-		//count = 0;;
+		delete[] dataArray;
+		capacity = 0;
+		count = 0;
 	}
 
-	void add(int item)
+	/// @brief adds a new item to the tree and grows, if necessary
+	void add(int data)
 	{
 		int position = 0;
 
 		//root node? array[0] != nullptr
 		Node currentNode = dataArray[position];
 
-		if (currentNode.data == -1)
+		//if nothing in the array at start position then insert data and return
+		if (currentNode.data == this->defaultValue)
 		{
-			currentNode.data = item;
+			dataArray[position].data = data;
+			count++;
 			return;
 		}
 		else
 		{
-			while (currentNode.data != -1)
+			//keep looking left/right to find an empty array cell
+			while (currentNode.data != this->defaultValue)
 			{
 				//compare new data to currentNode.data
-				if (item <= currentNode.data)
+				//note we are doing a <= comparison so whatever the data is (e.g. Student) will need to override the <= operator
+				if (data <= currentNode.data)
 					position = 2 * position + 1;				//if less, go left
 				else
 					position = 2 * position + 2; 				//if more, go right
 
-				if (position >= capacity)
-					throw "No more space in tree - here is where we call a grow() function!";
+				//check that we dont overrun the end of the array and grow
+				if (position >= capacity) {
+					Node* grownDataArray = grow(dataArray, capacity, 2 * position);
+					//set the new capacity otherwise the print wont show the new grown content
+					this->capacity = 2 * position;
+					//free the old array
+					delete[] dataArray;
+					//make dataArray point at new grown array
+					dataArray = grownDataArray;
+
+					//we used to just throw an exception but we have grow() so use it!
+					//throw "No more space in tree - here is where we call a grow() function!";
+				}
 
 				//move down to left/right and repeat until we find an empty node (i.e. data == -1)
 				currentNode = dataArray[position];
 			}
 
 			//found an empty node on the correct side and so add the data
-			currentNode.data = item;
+			dataArray[position].data = data;
+
+			count++;
 		}
 	}
 
-	//print
+	/// @brief print
+	void print() {
+		if (capacity == 0 || count == 0)
+			return;
 
-	//clear
+		for (int i = 0; i < capacity; i++)
+			//print the contents and give each row 16 characters - just to make things look nice!
+			cout << "[" << std::setw(2) << i << "]| " << std::setw(16) << dataArray[i] << " | " << endl;
+	}
 
-	//count
+	/// @brief clear
+	void clear() {
+		for (int i = 0; i < capacity; i++)
+			dataArray[i] = defaultValue;
+	}
+
+	/// @brief get capacity
+	int getCount() {
+		return this->count;
+	}
+
+	/// @brief takes a pointer to the first element in an array of Nodes and grow and return grown node (+ old contents)
+	Node* grow(Node* tree, int capacity, int newCapacity)
+	{
+		if (newCapacity <= capacity)
+			throw std::invalid_argument("newCapacity should be at larger than original to grow array!");
+
+		//make the new array
+		Node* grown = new Node[newCapacity];
+
+		//copy the data from the original into the grown
+		for (int i = 0; i < capacity; i++) {
+			grown[i] = tree[i];
+		}
+
+		//initialize the unused/unset values in the grown
+		for (int i = capacity; i < newCapacity; i++) {
+			grown[i] = Node(this->defaultValue);
+		}
+
+		return grown;
+	}
 };
-/*
-* whatever struct or class that we store in our tree needs to provide some way to compare its data (e.g. student age) with
-* the data in a node/cell in the array. therefore, we need to impplement <, >= or <=, > operators for comparison
-class Student {
-public:
-	int age;
-
-	bool operator<=(const Student& rhs);
-	bool operator>(const Student& rhs);
-	bool operator==(const Student& rhs);
-	bool operator!=(const Student& rhs);
-};
-*/
-
-//
-//template <class T>
-//class BinaryTree
-//{
-//public:
-//	//array
-//	int size = 0;
-//
-//	BinaryTree();
-//	BinaryTree(int numLevels);
-//	void add(T item);
-//	void remove();
-//	~BinaryTree();
-//};//
-//template<class T>
-//inline BinaryTree<T>::BinaryTree()
-//{
-//}
-//
-//template<class T>
-//inline BinaryTree<T>::BinaryTree(int numLevels)
-//{
-//}
-//
-//template<class T>
-//inline void BinaryTree<T>::add(T item)
-//{
-//}
-//
-//template<class T>
-//inline void BinaryTree<T>::remove()
-//{
-//}
-//
-//template<class T>
-//inline BinaryTree<T>::~BinaryTree()
-//{
-//}
